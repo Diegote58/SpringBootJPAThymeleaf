7df9cbcd028d7df263389719899c9765d2612492
diego gimenez <diegote58@gmail.com>
2017-08-02 16:13:51 -0300

    Se securiza configura el Login con Spring Security

--- src/main/java/com/sistema/controller/LoginController.java
+++ src/main/java/com/sistema/controller/LoginController.java
@@ -5,23 +5,14 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sistema.constant.ViewConstant;
-import com.sistema.model.UserCredential;
 
 @Controller
 public class LoginController {
 
 	private static final Log LOGGER = LogFactory.getLog(LoginController.class);
-
-	@GetMapping("/")
-	public String redirectToLogin() {
-		return "redirect:/login";
-	}
 
 	@GetMapping("/login")
 	public String showLoginForm(Model model, @RequestParam(name = "error", required = false) String error,
@@ -29,21 +20,17 @@ public class LoginController {
 		LOGGER.info("METHOD: showLoginForm() -- PARAMS: error=" + error + ", logout=" + logout);
 		model.addAttribute("error", error);
 		model.addAttribute("logout", logout);
-		model.addAttribute("userCredentials", new UserCredential());
 		LOGGER.info("returning to login view");
 		return ViewConstant.LOGIN;
 	}
 
-	@PostMapping("/logincheck")
-	public String loginCheck(@ModelAttribute(name = "userCredentials") UserCredential userCredential) {
-
-		LOGGER.info("METHOD: loginCheck() -- PARAMS: " + userCredential.toString());
-		if (userCredential.getUsername().equals("user") && userCredential.getPassword().equals("user")) {
-			LOGGER.info("Returning to contacts view");
-			// return ViewConstant.CONTACTS;
-			return "redirect:/contacts/showContacts";
-		}
-		LOGGER.info("Returning to login?error");
-		return "redirect:/login?error";
+	@GetMapping({ "/loginsuccess", "/" })
+	public String loginCheck() {
+		LOGGER.info("METHOD: loginCheck()");
+		LOGGER.info("Returning to contacts view");
+		// return ViewConstant.CONTACTS;
+		return "redirect:/contacts/showContacts";
 	}
+	
+
 }
