0507a918cf9468b5b9b21c9866f51339d8c7dc55
diego gimenez <diegote58@gmail.com>
2017-08-01 21:22:31 -0300

    Se agregan logs en el LoginController

--- src/main/java/com/sistema/controller/LoginController.java
+++ src/main/java/com/sistema/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.sistema.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,9 @@ import com.sistema.model.UserCredential;
 @Controller
 public class LoginController {
 
+	private static final Log LOGGER = LogFactory.getLog(LoginController.class);
+	
+	
 	@GetMapping("/")
 	public String redirectToLogin() {
 		return "redirect:/login";
@@ -22,18 +27,23 @@ public class LoginController {
 	public String showLoginForm(Model model,
 			@RequestParam(name = "error", required = false) String error,
 			@RequestParam(name = "logout", required = false) String logout) {
+		LOGGER.info("METHOD: showLoginForm() -- PARAMS: error=" + error + ", logout=" + logout);
 		model.addAttribute("error", error);
 		model.addAttribute("logout", logout);
 		model.addAttribute("userCredentials", new UserCredential());
+		LOGGER.info("returning to login view");
 		return "login";
 	}
 
 	@PostMapping("/logincheck")
 	public String loginCheck(@ModelAttribute(name = "userCredentials") UserCredential userCredential) {
 		
+		LOGGER.info("METHOD: loginCheck() -- PARAMS: " + userCredential.toString());
 		if (userCredential.getUsername().equals("user") && userCredential.getPassword().equals("user")) {
+			LOGGER.info("Returning to contacts view");
 			return "contacts";
 		}
+		LOGGER.info("Returning to login?error");
 		return "redirect:/login?error";
 	}
 }
